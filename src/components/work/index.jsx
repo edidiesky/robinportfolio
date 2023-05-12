35beff7bde96673767fbfbde4f2ab94ca58c290a
edidiesky <essienedidiong1000@gmail.com>
2023-05-12 17:39:39 +0100

    NEW FIXES

--- src/components/work/index.jsx
+++ src/components/work/index.jsx
@@ -14,38 +14,37 @@ export default function News() {
           SOME OF MY WORKS
         </h3>
       </div>
-      <div
-        data-aos="fade"
-        data-aos-duration="1600"
-        className="w-100 auto gap-2 newswp"
-      >
-        <div className="headerWrapper w-100">
-          <div className="imagegradient"></div>
-          <img
-            src="https://uploads-ssl.webflow.com/5b0eb11723332d2455b73469/5b57ecf81371d7ac0ac7f304_boosted-2018-bg.jpg"
-            alt=""
-            className="imageWrapper"
-          />
-          <div className="herowrapper h-100 flex justify-center column gap-3 w-85 auto">
-            <h4
-              style={{ fontWeight: "400" }}
-              className="family2 fs-16 text-grey"
+      <div className="w-100 auto gap-2 newswp">
+        {work.map((x, index) => {
+          return (
+            <div
+              data-aos="fade"
+              data-aos-duration="1600"
+              data-aos-dalay={index * 300}
+              className="headerWrapper w-100"
+              key={index}
             >
-              Ful-Stack Development
-            </h4>
-            <h2 className="fs-46 w-50 family2 text-light text-white">
-              WHERE TRAVEL MEETS PHOTOGRAPHY
-            </h2>
-            <h4 className="family1 fs-14 text-white">
-              I was lucky enough to work with the good people at Moment to
-              explore ways to improve booking trips on their travel site.
-            </h4>
-            <div className="w-100 flex item-center gap-2">
-              <div className="btn fs-16 family2 py-1">View Demo</div>
-              <div className="btn fs-16 family2 py-1">View Code</div>
+              <div className="imagegradient"></div>
+              <img src={x.image} alt="" className="imageWrapper" />
+              <div className="herowrapper h-100 flex justify-center column gap-3 w-85 auto">
+                <h4
+                  style={{ fontWeight: "400" }}
+                  className="family2 fs-16 text-secondary"
+                >
+                  Ful-Stack Development
+                </h4>
+                <h2 className="fs-46 w-50 family2 text-light text-white">
+                  {x.title}
+                </h2>
+                <h4 className="family1 fs-14 text-white">{x.description}</h4>
+                <div className="w-100 flex item-center gap-2">
+                  <div className="btn fs-14 family2 py-1">View project Demo</div>
+                  <div className="btn fs-14 family2 py-1">View project Code</div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </NewsContent>
   );
@@ -58,26 +57,32 @@ const NewsContent = styled.div`
     font-family: "Bebas Neue", sans-serif;
   }
   .btn {
-    padding: 0.6rem 2rem;
+    padding: 1.2rem 3rem;
   }
   .herowrapper {
     z-index: 6000;
+    gap: 3rem;
+    @media (max-width: 480px) {
+      gap: 5rem;
+    }
     h4 {
       width: 40%;
       @media (max-width: 780px) {
         width: 60%;
       }
       @media (max-width: 480px) {
-        width: 70%;
+        width: 90%;
       }
     }
     h2 {
-      line-height: 44px;
+      line-height: 49px;
       font-weight: 500;
       font-size: 60px;
       width: 50%;
       @media (max-width: 780px) {
         width: 60%;
+        line-height: 50px;
+        font-size: 40px;
       }
       @media (max-width: 480px) {
         width: 70%;
@@ -135,7 +140,7 @@ const NewsContent = styled.div`
   .headerWrapper {
     position: relative;
     min-height: 100vh;
-    padding: 6rem 0;
+    padding: 16rem 0;
     display: grid;
     place-items: center;
   }
@@ -151,44 +156,12 @@ const NewsContent = styled.div`
     z-index: 400;
     background-image: linear-gradient(
       to bottom,
-      rgba(0, 0, 0, 0.1),
-      rgba(0, 0, 0, 0.1),
-      rgba(0, 0, 0, 0.1),
-      rgba(0, 0, 0, 0.1),
-      rgba(0, 0, 0, 0.1)
+      rgba(0, 0, 0, 0.3),
+      rgba(0, 0, 0, 0.3),
+      rgba(0, 0, 0, 0.3),
+      rgba(0, 0, 0, 0.3),
+      rgba(0, 0, 0, 0.3)
     );
-  }
-  /* 
-  .newswp {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-gap: 3rem;
-    .imageContent {
-      flex: 1;
-    }
-    .NewsRight {
-      flex: 1;
-      padding: 15rem 0;
-      background-color: #000;
-      transition: all 0.7s;
-      &:hover {
-        transform: scale(1.02);
-        box-shadow: 0 2px 3px rgba(0, 0, 0, 0.2);
-      }
-      .NewsRightC {
-        width: 70%;
-      }
-    }
-    @media (max-width: 980px) {
-      grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    }
-  } */
-
-  .CriticsBottom {
-    display: grid;
-    padding-top: 10rem;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-    grid-gap: 5rem;
   }
 `;
 
--- src/data/work.jsx
+++ src/data/work.jsx
@@ -1,6 +1,102 @@
+// export const work = [
+//   {
+//     image: "https://v1.brittanychiang.com/img/nuwit.png",
+//     description:
+//       "Crowd DJ is a web app that allows people who are at a party or social gathering with a DJ (who uses Spotify) to request songs via their smartphones. The web app allows people to see the currently playing song, request songs, upvote songs, and rate songs so the DJ can see how the crowd is feeling and act accordingly. In other words, Crowd DJ is essentially a crowdsourced music queue.",
+//     tools: [
+//       "REACT.JS",
+//       "CSS",
+//       "styled-components",
+//       "NODEJS",
+//       "EXPRESSJS",
+//       "MONGODB",
+//       "HTML",
+//     ],
+//     title:'Social Media Website'
+//   },
+//   {
+//     image:
+//       "https://v2.brittanychiang.com/img/portfolio/screentime/screentime-2.jpg",
+//     description:
+//       "Crowd DJ is a web app that allows people who are at a party or social gathering with a DJ (who uses Spotify) to request songs via their smartphones. The web app allows people to see the currently playing song, request songs, upvote songs, and rate songs so the DJ can see how the crowd is feeling and act accordingly. In other words, Crowd DJ is essentially a crowdsourced music queue.",
+//     tools: [
+//       "REACT.JS",
+//       "CSS",
+//       "styled-components",
+//       "NODEJS",
+//       "EXPRESSJS",
+//       "MONGODB",
+//       "HTML",
+//     ],
+//     title:'Car E-commerce website'
+//   },
+//   {
+//     image: "https://v2.brittanychiang.com/img/portfolio/crowddj/crowddj.jpg",
+//     description:
+//       "Crowd DJ is a web app that allows people who are at a party or social gathering with a DJ (who uses Spotify) to request songs via their smartphones. The web app allows people to see the currently playing song, request songs, upvote songs, and rate songs so the DJ can see how the crowd is feeling and act accordingly. In other words, Crowd DJ is essentially a crowdsourced music queue.",
+//     tools: [
+//       "NEXT.JS",
+//       "CSS",
+//       "styled-components",
+//       "NODEJS",
+//       "EXPRESSJS",
+//       "MONGODB",
+//       "HTML",
+//     ],
+//     title:'Fiverr Clone'
+//   },
+//   {
+//     image:
+//       "https://v2.brittanychiang.com/img/portfolio/websitev1/websitev1.jpg",
+//     description:
+//       "Crowd DJ is a web app that allows people who are at a party or social gathering with a DJ (who uses Spotify) to request songs via their smartphones. The web app allows people to see the currently playing song, request songs, upvote songs, and rate songs so the DJ can see how the crowd is feeling and act accordingly. In other words, Crowd DJ is essentially a crowdsourced music queue.",
+//     tools: [
+//       "REACT.JS",
+//       "CSS",
+//       "styled-components",
+//       "NODEJS",
+//       "EXPRESSJS",
+//       "MONGODB",
+//       "HTML",
+//     ],
+//     title:'Restaurant Marketplace'
+//   },
+//   {
+//     image: "https://v2.brittanychiang.com/img/portfolio/crowddj/crowddj.jpg",
+//     description:
+//       "Crowd DJ is a web app that allows people who are at a party or social gathering with a DJ (who uses Spotify) to request songs via their smartphones. The web app allows people to see the currently playing song, request songs, upvote songs, and rate songs so the DJ can see how the crowd is feeling and act accordingly. In other words, Crowd DJ is essentially a crowdsourced music queue.",
+//     tools: [
+//       "REACT.JS",
+//       "CSS",
+//       "styled-components",
+//       "NODEJS",
+//       "EXPRESSJS",
+//       "MONGODB",
+//       "HTML",
+//     ],
+//     title:'Job Board'
+//   },
+//   {
+//     image: "https://v2.brittanychiang.com/img/portfolio/crowddj/crowddj.jpg",
+//     description:
+//       "Crowd DJ is a web app that allows people who are at a party or social gathering with a DJ (who uses Spotify) to request songs via their smartphones. The web app allows people to see the currently playing song, request songs, upvote songs, and rate songs so the DJ can see how the crowd is feeling and act accordingly. In other words, Crowd DJ is essentially a crowdsourced music queue.",
+//     tools: [
+//       "REACT.JS",
+//       "CSS",
+//       "styled-components",
+//       "NODEJS",
+//       "EXPRESSJS",
+//       "MONGODB",
+//       "HTML",
+//     ],
+//     title:'Job Board'
+//   },
+// ];
+
 export const work = [
   {
-    image: "https://v1.brittanychiang.com/img/nuwit.png",
+    image:
+      "https://uploads-ssl.webflow.com/5b0eb11723332d2455b73469/5e04efbf0b1ac58fadeb3a64_boosted-ux-hero.jpg",
     description:
       "Crowd DJ is a web app that allows people who are at a party or social gathering with a DJ (who uses Spotify) to request songs via their smartphones. The web app allows people to see the currently playing song, request songs, upvote songs, and rate songs so the DJ can see how the crowd is feeling and act accordingly. In other words, Crowd DJ is essentially a crowdsourced music queue.",
     tools: [
@@ -12,11 +108,11 @@ export const work = [
       "MONGODB",
       "HTML",
     ],
-    title:'Social Media Website'
+    title: "DEVELOPING A RESPONSIVE NFT WEBSITE",
   },
   {
     image:
-      "https://v2.brittanychiang.com/img/portfolio/screentime/screentime-2.jpg",
+      "https://uploads-ssl.webflow.com/5b0eb11723332d2455b73469/5b57ecf81371d7ac0ac7f304_boosted-2018-bg.jpg",
     description:
       "Crowd DJ is a web app that allows people who are at a party or social gathering with a DJ (who uses Spotify) to request songs via their smartphones. The web app allows people to see the currently playing song, request songs, upvote songs, and rate songs so the DJ can see how the crowd is feeling and act accordingly. In other words, Crowd DJ is essentially a crowdsourced music queue.",
     tools: [
@@ -28,26 +124,11 @@ export const work = [
       "MONGODB",
       "HTML",
     ],
-    title:'Car E-commerce website'
-  },
-  {
-    image: "https://v2.brittanychiang.com/img/portfolio/crowddj/crowddj.jpg",
-    description:
-      "Crowd DJ is a web app that allows people who are at a party or social gathering with a DJ (who uses Spotify) to request songs via their smartphones. The web app allows people to see the currently playing song, request songs, upvote songs, and rate songs so the DJ can see how the crowd is feeling and act accordingly. In other words, Crowd DJ is essentially a crowdsourced music queue.",
-    tools: [
-      "NEXT.JS",
-      "CSS",
-      "styled-components",
-      "NODEJS",
-      "EXPRESSJS",
-      "MONGODB",
-      "HTML",
-    ],
-    title:'Fiverr Clone'
+    title: "Social Media Website",
   },
   {
     image:
-      "https://v2.brittanychiang.com/img/portfolio/websitev1/websitev1.jpg",
+      "https://uploads-ssl.webflow.com/5b0eb11723332d2455b73469/5ba8028a9ad787a0531f56cc_kore-cs-bg.png",
     description:
       "Crowd DJ is a web app that allows people who are at a party or social gathering with a DJ (who uses Spotify) to request songs via their smartphones. The web app allows people to see the currently playing song, request songs, upvote songs, and rate songs so the DJ can see how the crowd is feeling and act accordingly. In other words, Crowd DJ is essentially a crowdsourced music queue.",
     tools: [
@@ -59,10 +140,11 @@ export const work = [
       "MONGODB",
       "HTML",
     ],
-    title:'Restaurant Marketplace'
+    title: " Cloneing a functional Social Media Website",
   },
   {
-    image: "https://v2.brittanychiang.com/img/portfolio/crowddj/crowddj.jpg",
+    image:
+      "https://uploads-ssl.webflow.com/5b0eb11723332d2455b73469/6362d058c159b6998224ea81_220801_KYSON-DANA_EASTERN_IDAHO-.jpeg",
     description:
       "Crowd DJ is a web app that allows people who are at a party or social gathering with a DJ (who uses Spotify) to request songs via their smartphones. The web app allows people to see the currently playing song, request songs, upvote songs, and rate songs so the DJ can see how the crowd is feeling and act accordingly. In other words, Crowd DJ is essentially a crowdsourced music queue.",
     tools: [
@@ -74,10 +156,11 @@ export const work = [
       "MONGODB",
       "HTML",
     ],
-    title:'Job Board'
+    title: "Social Media Website",
   },
   {
-    image: "https://v2.brittanychiang.com/img/portfolio/crowddj/crowddj.jpg",
+    image:
+      "https://uploads-ssl.webflow.com/5b0eb11723332d2455b73469/6362d058c159b6998224ea81_220801_KYSON-DANA_EASTERN_IDAHO-.jpeg",
     description:
       "Crowd DJ is a web app that allows people who are at a party or social gathering with a DJ (who uses Spotify) to request songs via their smartphones. The web app allows people to see the currently playing song, request songs, upvote songs, and rate songs so the DJ can see how the crowd is feeling and act accordingly. In other words, Crowd DJ is essentially a crowdsourced music queue.",
     tools: [
@@ -89,6 +172,6 @@ export const work = [
       "MONGODB",
       "HTML",
     ],
-    title:'Job Board'
+    title: "DEVELOPING A CAR SALES WEBSITE.",
   },
 ];
